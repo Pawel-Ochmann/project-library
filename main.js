a72ca19524b5a108f8ccc992b2b240c56a8f7c32
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-05-08 11:17:50 +0200

    Create function that append taking data and create table, need to add its simpler version that will add only one book

--- main.js
+++ main.js
@@ -13,11 +13,34 @@ function getData() {
 
   const anotherBook = new Book(title, author, pages, read);
   myLibrary.push(anotherBook);
-  console.log(myLibrary);
+  createTable();
 }
 
 function createTable() {
-    
+  if (myLibrary.length === 0) return;
+  const body = document.querySelector("body");
+  const table = document.createElement("table");
+  const firstRow = document.createElement("tr");
+  firstRow.innerHTML += `             
+                <th>Nr</th>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Pages</th>
+                <th>Read</th>`;
+  table.appendChild(firstRow);
+  for (book of myLibrary) {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+                <th>${myLibrary.indexOf(book) + 1}</th>
+                <th>${book.title}</th>
+                <td>${book.author}</td>
+                <td>${book.pages}</td>
+                <td>${book.read}</td>
+                <td><button>Delete</button></td>
+    `;
+    table.appendChild(row);
+  }
+  body.appendChild(table);
 }
 
 function Book(title, author, pages, read) {
@@ -25,9 +48,9 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-//   this.info = function () {
-//     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`;
-//   };
+  //   this.info = function () {
+  //     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`;
+  //   };
 }
 
 const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
