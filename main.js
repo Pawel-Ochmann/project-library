a34395713f3334dd50be2a15e293846d7ec37de7
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-05-12 10:33:37 +0200

    Style everything with css

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 let tableAppeared = false;
-const body = document.querySelector("body");
-const table = document.createElement("table");
-const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
-const diuna = new Book("The Diuna", "Frank Herbert", 784, false);
-const solaris = new Book("Solaris", "Stanisław Lem", 204, true);
+const body = document.querySelector('body');
+const table = document.createElement('table');
+const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
+const diuna = new Book('The Diuna', 'Frank Herbert', 784, false);
+const solaris = new Book('Solaris', 'Stanisław Lem', 204, true);
 
-let myLibrary = [];
+const myLibrary = [];
 myLibrary.push(hobbit, diuna, solaris);
 createTable();
 
-const submitButton = document.querySelector("button[type=submit");
-submitButton.addEventListener("click", (e) => {
+const submitButton = document.querySelector('button[type=submit');
+submitButton.addEventListener('click', (e) => {
   e.preventDefault();
   getData();
 });
 
 function getData() {
-  const title = document.querySelector("#title").value;
-  const author = document.querySelector("#author").value;
-  const pages = document.querySelector("#pages").value;
-  const read = document.querySelector("#read").checked;
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const pages = document.querySelector('#pages').value;
+  const read = document.querySelector('#read').checked;
 
   const anotherBook = new Book(title, author, pages, read);
   myLibrary.push(anotherBook);
@@ -28,8 +28,8 @@ function getData() {
 
 function createTable() {
   if (myLibrary.length === 0) return;
-  table.innerHTML = ``;
-  const firstRow = document.createElement("tr");
+  table.innerHTML = '';
+  const firstRow = document.createElement('tr');
   firstRow.innerHTML += `             
                 <th>Nr</th>
                 <th>Title</th>
@@ -38,27 +38,20 @@ function createTable() {
                 <th>Read</th>`;
   table.appendChild(firstRow);
   for (book of myLibrary) {
-    let readChecked = "";
+    let readChecked = '';
     console.log(book.read);
-    if (book.read===true) {
-      readChecked = "checked";
+    if (book.read === true) {
+      readChecked = 'checked';
     }
 
-    const row = document.createElement("tr");
+    const row = document.createElement('tr');
     row.innerHTML = `
                 <th>${myLibrary.indexOf(book) + 1}</th>
                 <th>${book.title}</th>
                 <td>${book.author}</td>
                 <td>${book.pages}</td>
-                <td>${
-                  book.read
-                }<input type='checkbox' class="readCheck" data-key="${myLibrary.indexOf(
-      book
-    )}" ${readChecked}></td>
-                <td><button class="delete" data-key="${myLibrary.indexOf(
-                  book
-                )}">Delete</button></td>
-    `;
+                <td>${book.read}<input type='checkbox' class="readCheck" data-key="${myLibrary.indexOf(book)}" ${readChecked}></td>
+                <td><button class="delete" data-key="${myLibrary.indexOf(book)}">Delete</button></td>`;
     table.appendChild(row);
   }
   body.appendChild(table);
@@ -78,14 +71,14 @@ function Book(title, author, pages, read) {
 }
 
 function getDeleteOption() {
-  const deleteButtons = document.querySelectorAll(".delete");
+  const deleteButtons = document.querySelectorAll('.delete');
   deleteButtons.forEach((button) => {
-    button.addEventListener("click", (e) => {
+    button.addEventListener('click', () => {
       console.log(myLibrary);
       myLibrary.splice(button.dataset.key, 1);
       console.log(myLibrary);
       if (myLibrary.length === 0) {
-        table.innerHTML = ``;
+        table.innerHTML = '';
         tableAppeared = false;
         return;
       }
@@ -95,9 +88,9 @@ function getDeleteOption() {
 }
 
 function addReadCheckbox() {
-  const readCheckInputs = document.querySelectorAll(".readCheck");
+  const readCheckInputs = document.querySelectorAll('.readCheck');
   readCheckInputs.forEach((input) => {
-    input.addEventListener("click", (e) => {
+    input.addEventListener('click', () => {
       myLibrary[input.dataset.key].read = input.checked;
       createTable();
     });
